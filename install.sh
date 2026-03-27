ab0f67c1a6094de51b4e93dd639204e2e5f67c5a
codeBuddha <jamwujustyle@gmail.com>
2026-03-27 15:10:55 +0500

    add ble

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ sudo apt-get install -y tree pgcli psmisc lsof
 
 # add ble sh in the future here
 #..
-
-
+if [ ! -d "$HOME/.local/share/blesh" ]; then
+    git clone --recursive https://github.com/akinomyoga/ble.sh.git "$HOME/.local/share/blesh"
+    make -C "$HOME/.local/share/blesh" install PREFIX="$HOME/.local"
+fi
