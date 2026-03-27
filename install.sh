4c1fc8efa92f20cb5b4c14736fe3b649e295a571
codeBuddha <jamwujustyle@gmail.com>
2026-03-27 15:29:30 +0500

    override bashrc

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# 1. FORCE the symlink (This is the fix)
+# This points the system .bashrc to YOUR version in the repo
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
+# 2. Install your required tools
 sudo apt-get update
 sudo apt-get install -y tree pgcli psmisc lsof
 
-# add ble sh in the future here
-#..
+# 3. Optional: Install ble.sh for syntax highlighting
 if [ ! -d "$HOME/.local/share/blesh" ]; then
     git clone --recursive https://github.com/akinomyoga/ble.sh.git "$HOME/.local/share/blesh"
+    # Note: We use 'make' but it might need to be installed too
+    sudo apt-get install -y make
     make -C "$HOME/.local/share/blesh" install PREFIX="$HOME/.local"
 fi
