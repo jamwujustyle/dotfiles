2d85f9be01c97b99f319214b58a81cf36e5ddc02
codeBuddha <jamwujustyle@gmail.com>
2026-03-27 20:58:00 +0500

    upd

--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# 1. FORCE the symlink (This is the fix)
-# This points the system .bashrc to YOUR version in the repo
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+# 1. Get the absolute path of the dotfiles directory
+DOTFILES_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# 2. Install your required tools
+# 2. FORCE the symlink using the absolute path
+# This ensures it points to /home/codespace/dotfiles/.bashrc
+ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
+
+# 3. Fix permissions just in case
+chmod 644 "$DOTFILES_DIR/.bashrc"
+
+# 4. Install tools
 sudo apt-get update
 sudo apt-get install -y tree pgcli psmisc lsof
 
-# 3. Optional: Install ble.sh for syntax highlighting
+# 5. Ble.sh install
 if [ ! -d "$HOME/.local/share/blesh" ]; then
     git clone --recursive https://github.com/akinomyoga/ble.sh.git "$HOME/.local/share/blesh"
-    # Note: We use 'make' but it might need to be installed too
     sudo apt-get install -y make
     make -C "$HOME/.local/share/blesh" install PREFIX="$HOME/.local"
 fi
